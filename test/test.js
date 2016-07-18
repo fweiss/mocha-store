d93a226a001d6ed62d8495bd820dbaefef280a0e
Frank E Weiss <feweiss@gmail.com>
2016-07-18 01:48:28 -0700

    Refactor: clean up the code for readability

--- test/test.js
+++ test/test.js
@@ -3,12 +3,11 @@ var expect = require('chai').expect;
 var request = require('supertest');
 
 describe('coffee', function() {
-    var server = 'http://localhost:8001';
-    it('is ready', function(done) {
-        request(server).post('/orders').send().expect(201).end(function(err, res) {
-            if (err) {
-                throw err;
-            }
+    var api = request('http://localhost:8001');
+    it('adds new order', function(done) {
+        api.post('/orders').send().end(function(err, res) {
+            expect(err).to.not.exist;
+            expect(res.status).to.equal(201);
             expect(res.body.order.drink).to.equal('latte');
             done();
         });
