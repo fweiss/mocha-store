008a385b054857532f222eecd868365564319b0d
Frank E Weiss <feweiss@gmail.com>
2017-02-05 10:46:35 -0800

    Added modify order Added musings on hypermedia discoverability

--- README.md
+++ README.md
@@ -14,3 +14,14 @@ chatty if it's implemented in a smart client.
 The type was given as 'application/json', but this doesn't really tell the client the schema of the domain
 object that is involved in the indicated resource.
 
+### Self-describing documents
+
+Assuming that any API call provides sufficient hypermedia links to navigate the entire application state, there's still
+the question of discovering the document types. For example, by convention, one can assume that /v1/orders would allow
+a POST to create a new order. But how would the structure of the valid POST body be discovered? Certainly not by trial and error.
+
+Zac provides an older but interesting inquiry into using OPTIONS to provide a resource's metadata: https://www.infoq.com/articles/webber-rest-workflow
+
+An alternate, which overcomes the non-cacheability of OPTIONS, is the HEAD method: https://www.mnot.net/blog/2012/10/29/NO_OPTIONS
+
+How about having the body of OPTIONS provide the media types by which the resource can be accessed.
--- test/test.js
+++ test/test.js
@@ -4,40 +4,63 @@ var request = require('supertest');
 describe('coffee store', function() {
     var api = request('http://localhost:8001');
 
-    describe('create order', function() {
-        it('can order americano', function(done) {
-            var order = { order: { drink: 'americano' } };
-            api.post('/orders').send(order).end(function(err, res) {
-                expect(err).to.not.exist;
-                expect(res.status).to.equal(201);
-                expect(res.body.order.id).to.equal(1);
-                expect(res.body.order.drink).to.equal('americano');
-                expect(res.body.order.cost).to.equal(3);
-                expect(res.body.order.next).to.exist;
-                done();
+    describe('orders', function() {
+        describe('create', function() {
+            it('can order americano', function(done) {
+                var order = { order: { drink: 'americano' } };
+                api.post('/orders').send(order).end(function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.status).to.equal(201);
+                    expect(res.body.order.id).to.equal(1);
+                    expect(res.body.order.drink).to.equal('americano');
+                    expect(res.body.order.cost).to.equal(3);
+                    expect(res.body.order.next).to.exist;
+                    done();
+                });
+            });
+            it('can order latte', function(done) {
+                var order = { order: { drink: 'latte' } };
+                api.post('/orders').send(order).end(function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.status).to.equal(201);
+                    expect(res.body.order.drink).to.equal('latte');
+                    done();
+                });
+            });
+            it('has hypermedia link', function(done) {
+                var order = { order: { drink: 'latte' } };
+                api.post('/orders').send(order).end(function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.status).to.equal(201);
+                    expect(res.body.order.next).to.exist;
+                    expect(res.body.order.next.rel).to.equal('payment');
+                    expect(res.body.order.next.href).to.equal('http://localhost:8001/payment/order/1');
+                    expect(res.body.order.next.type).to.equal('application/json');
+                    done();
+                });
+            });
+         });
+        describe('modify', function() {
+            it('can query existence', function(done) {
+                api.options('/orders/1').send().end(function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.status).to.equal(200);
+                    expect(res.headers['allow']).to.exist;
+                    expect(res.headers['allow']).to.equal('GET, PUT');
+                    done();
+                });
+            });
+            it('can add shot', function(done) {
+                var partialOrder = { order: { additions: 'tor' }};
+                api.put('/orders/1').send(partialOrder).set('Expect', '100-Continue').end(function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.status).to.equal(200);
+                    expect(res.body.order.additions).to.equal('tor');
+                    done();
+                });
             });
         });
-        it('can order latte', function(done) {
-            var order = { order: { drink: 'latte' } };
-            api.post('/orders').send(order).end(function(err, res) {
-                expect(err).to.not.exist;
-                expect(res.status).to.equal(201);
-                expect(res.body.order.drink).to.equal('latte');
-                done();
-            });
-        });
-        it('has hypermedia link', function(done) {
-            var order = { order: { drink: 'latte' } };
-            api.post('/orders').send(order).end(function(err, res) {
-                expect(err).to.not.exist;
-                expect(res.status).to.equal(201);
-                expect(res.body.order.next).to.exist;
-                expect(res.body.order.next.rel).to.equal('payment');
-                expect(res.body.order.next.href).to.equal('http://localhost:8001/payment/order/1');
-                expect(res.body.order.next.type).to.equal('application/json');
-                done();
-            });
-        });
-
     });
+
+
  });
