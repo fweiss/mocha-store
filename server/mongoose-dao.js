85024d8f6a0faa67ee25ef01c2e56600aafcfe5d
Frank E Weiss <feweiss@gmail.com>
2020-08-16 18:30:19 -0700

    WIP integrate mongoose dao, need to use promise

--- server/mongoose-dao.js
+++ server/mongoose-dao.js
@@ -1,3 +1,25 @@
+var Mongoose = require('mongoose').Mongoose;
+var mongoose = new Mongoose();
+const schemas = require('../server/schemas')
+
+var orderScheme = new mongoose.Schema(schemas.orderSchema);
+Order = mongoose.model('Order', orderScheme);
+
 module.exports = {
-    getOrders: () => {}
+
+    getOrders: () => {},
+    addOrder: (order) => {
+        var Order = mongoose.model('Order')
+        Order.create(order, function(err, order) {
+            if (err) {
+                // there may also be 500, non validation errors
+                res.status(400)
+                res.send(err)
+                return
+            }
+            res.status(201)
+            res.send(order)
+        })
+        return {}}
+
 }
