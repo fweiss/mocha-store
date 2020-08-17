9a202de58cc4278f764dc170c7f29dc2cfe46b08
Frank E Weiss <feweiss@gmail.com>
2020-08-17 08:29:58 -0700

    Remove dead code

--- server/mongoose-dao.js
+++ server/mongoose-dao.js
@@ -23,18 +23,6 @@ module.exports = {
                 order: { drink: 'latte', cost: '3.00' }
             }
         }
-        // var Order = mongoose.model('Order')
-        // Order.create(order, function(err, order) {
-        //     if (err) {
-        //         // there may also be 500, non validation errors
-        //         res.status(400)
-        //         res.send(err)
-        //         return
-        //     }
-        //     res.status(201)
-        //     res.send(order)
-        // })
-        // return {}
     }
 
 }
